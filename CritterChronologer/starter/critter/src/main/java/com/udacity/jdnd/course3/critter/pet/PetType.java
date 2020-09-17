2209beb59553d64422edf8dcccfd7a3114126a17
Hyungsoo Ohk <i@ohk.kr>
2020-09-17 22:12:55 +0900

    delete ds_store

--- CritterChronologer/starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/PetType.java
+++
@@ -1,8 +0,0 @@
-package com.udacity.jdnd.course3.critter.pet;
-
-/**
- * A example list of pet type metadata that could be included on a request to create a pet.
- */
-public enum PetType {
-    CAT, DOG, LIZARD, BIRD, FISH, SNAKE, OTHER;
-}
